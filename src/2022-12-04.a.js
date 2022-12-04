96499b98e2ece12516e2a4769530cb1f46fe3175
WittRaider <domiwitt@gmx.net>
2022-12-04 07:06:59 +0100

    day 4

--- src/2022-12-04.a.js
+++ src/2022-12-04.a.js
@@ -1,5 +1,30 @@
 import { getLines, debug, exit } from '../lib/lib.js'
 
-getLines('./assets/2022.12.04.txt');
+function isContained(narrow,wide) {
+    return (narrow.low >= wide.low && narrow.high <= wide.high);
+}
 
-debug("test");
+function getPairs() {
+    let pairs = [];
+    getLines('./assets/2022.12.04.txt').forEach(line => {
+        line = line.trim();
+        let split = line.split(",");
+        let pair = {split:[{
+            low:parseInt(split[0].split("-")[0]),
+            high:parseInt(split[0].split("-")[1])
+        },{
+            low:parseInt(split[1].split("-")[0]),
+            high:parseInt(split[1].split("-")[1])
+        }]}
+        pair.isContained = isContained(pair.split[0],pair.split[1]) || isContained(pair.split[1],pair.split[0]);
+        pairs.push(pair);
+    });
+    return pairs;
+}
+
+let res = getPairs().reduce((total, pair) => {
+    let add = (pair.isContained)?1:0;
+    return total + add;
+},0);
+
+debug(res);
--- src/2022-12-04.b.js
+++ src/2022-12-04.b.js
@@ -1,3 +1,30 @@
 import { getLines, debug, exit } from '../lib/lib.js'
 
-getLines('./assets/2022.12.04.txt');
+function isOverlapping(a,b) {
+    return (a.high >= b.low && b.high >= a.low);
+}
+
+function getPairs() {
+    let pairs = [];
+    getLines('./assets/2022.12.04.txt').forEach(line => {
+        line = line.trim();
+        let split = line.split(",");
+        let pair = {split:[{
+            low:parseInt(split[0].split("-")[0]),
+            high:parseInt(split[0].split("-")[1])
+        },{
+            low:parseInt(split[1].split("-")[0]),
+            high:parseInt(split[1].split("-")[1])
+        }]}
+        pair.isOverlapping = isOverlapping(pair.split[0],pair.split[1]) || isOverlapping(pair.split[1],pair.split[0]);
+        pairs.push(pair);
+    });
+    return pairs;
+}
+
+let res = getPairs().reduce((total, pair) => {
+    let add = (pair.isOverlapping)?1:0;
+    return total + add;
+},0);
+
+debug(res);
