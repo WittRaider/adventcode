714361962dab35254caffef8e8e5a747bfeb89f4
WittRaider <domiwitt@gmx.net>
2022-12-06 06:40:28 +0100

    day 5 & 6

--- src/2022-12-05.b.js
+++ src/2022-12-05.b.js
@@ -1,5 +1,63 @@
 import { getLines, debug, exit } from '../lib/lib.js'
 
-getLines('./assets/2022.12.05.txt').forEach(line => {
-    
+let stacks = [[],[],[],[],[],[],[],[],[]];
+
+function parseLayer(line) {
+    if(line.indexOf("[")===-1) return;
+    let layer = [];
+    for(let i=0;i<9;i++) {
+        let item = line.substring(i*4,i*4+3);
+        item = item.trim();
+        if(!item) continue;
+        stacks[i].unshift(item);
+    }
+    return layer;
+}
+
+function parseIt() {
+    let instructions = [];
+    let inBody = false;
+    getLines('./assets/2022.12.05.txt').forEach(line => {
+        if(!line.trim()) {
+            inBody = true;
+        }
+
+        if(!inBody) {
+            parseLayer(line);
+        }
+
+        if(inBody) {
+            let split = line.trim().split(" ");
+            instructions.push({
+                move:split[1],
+                from:split[3],
+                to:split[5]
+            });
+
+        }
+    });
+    return {stacks:stacks,instructions:instructions};
+}
+
+function rearrange(stacks,instructions) {
+    instructions.forEach(instruction => {
+        let items = [];
+        for(let i=0;i<instruction.move;i++) {
+            let item = stacks[instruction.from-1].pop();
+            items.unshift(item);
+        }
+        for(let i=0;i<items.length;i++) {
+            stacks[instruction.to-1].push(items[i]);
+        }
+        
+    });
+    return stacks;
+}
+
+const parsed = parseIt();
+const rearragedStacks = rearrange(parsed.stacks,parsed.instructions);
+let topline = "";
+rearragedStacks.forEach(stack => {
+    topline += stack.pop();
 });
+debug(topline.replace(/\[/g,"").replace(/\]/g,""));
---
+++ src/2022-12-06.b.js
@@ -0,0 +1,29 @@
+import { getLines, debug, exit } from '../lib/lib.js'
+
+
+function areAllCharsUnique(chars) {
+    let unique = {};
+    for(let i=0;i<chars.length;i++) {
+        if(!unique[chars[i]]) unique[chars[i]] = 1;
+        else return false;
+    }
+    return true;
+}
+
+let lines = getLines('./assets/2022.12.06.txt');
+let line = lines[0];
+
+let buffer = '';
+for(let i=0;i<line.length;i++) {
+    let current = line[i];
+    if(buffer.length > 13) {
+ 
+        let lastFourOfBuffer = buffer.substring(buffer.length-14,buffer.length);
+        if(areAllCharsUnique(lastFourOfBuffer)) {
+            debug([lastFourOfBuffer,i,buffer]);
+            exit();
+        }
+   
+    }
+    buffer += line[i];
+}
